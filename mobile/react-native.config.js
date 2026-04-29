1f8170b4a9eacfe3a043449fc5c89b0187f6347e
JSmooVE3990 <Jurrand.Summerville.08@gmail.com>
2026-04-28 21:58:13 -0600

    chore: align dependencies with Expo 55 and fix build issues

---
+++ mobile/react-native.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  project: {
+    ios: {},
+    android: {},
+  },
+  root: path.resolve(__dirname),
+  reactNativePath: path.join(__dirname, '../node_modules/react-native'),
+};
